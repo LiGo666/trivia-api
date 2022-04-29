89fb6c131d36e6a63119309e89c4291b1ed27e1a
LiGo666 <christian.gotthardt@gmail.com>
2022-04-29 23:10:58 +0200

    updated response object

--- GetAllQuestions/index.js
+++ GetAllQuestions/index.js
@@ -1,9 +1,14 @@
 const questions = require("../questions");
 
 module.exports = async function(context, req) {
-  context.log("Request for all questions");
+  context.log("Request for all questions : ");
 
   context.res = {
-    body: questions
+    body: questions.map(q => {
+      return {
+        questions: q.question,
+        answers: q.incorrect_answers.concat([q.correct_answer]).sort()
+      };
+    })
   };
 };
